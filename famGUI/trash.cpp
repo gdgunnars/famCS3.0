3be0be8124f7561ecd64ca143b04a9a6744beb41
GD <gunnarg15@ru.is>
2015-12-11 19:49:31 +0000

    bæta við föllum í trash og viðeigandi tökkum í ui

--- famGUI/trash.cpp
+++ famGUI/trash.cpp
@@ -6,9 +6,33 @@ Trash::Trash(QWidget *parent) :
     ui(new Ui::Trash)
 {
     ui->setupUi(this);
+    getErased();
 }
 
 Trash::~Trash()
 {
     delete ui;
+}
+
+void Trash::getErased(){
+    ui->person_listWidget->clear();
+    ui->computer_listWidget->clear();
+    currentPersonVector = personD.listErased();
+    currentComputerVector = computerD.listErased();
+
+    for(unsigned int i = 0; i < currentPersonVector.size(); i++){
+        ui->person_listWidget->addItem(QString::fromStdString(currentPersonVector[i].getName()));
+    }
+
+    for(unsigned int i = 0; i < currentComputerVector.size(); i++){
+        ui->computer_listWidget->addItem(QString::fromStdString(currentComputerVector[i].getName()));
+    }
+}
+
+void Trash::on_pushButton_clicked()
+{
+    int rowId = ui->person_listWidget->currentRow();
+    int id = currentPersonVector[rowId].getId();
+    personD.restoreFromBin(id);
+    getErased();
 }
--- famGUI/trash.h
+++ famGUI/trash.h
@@ -2,6 +2,10 @@
 #define TRASH_H
 
 #include <QDialog>
+#include "cscientist.h"
+#include "computer.h"
+#include "Domain/computerdomain.h"
+#include "Domain/persondomain.h"
 
 namespace Ui {
 class Trash;
@@ -15,8 +19,22 @@ public:
     explicit Trash(QWidget *parent = 0);
     ~Trash();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::Trash *ui;
+    //vectors
+    vector<cScientist>  currentPersonVector;
+    vector<Computer>    currentComputerVector;
+
+    //objects
+    personDomain personD;
+    computerDomain computerD;
+
+    //funcs
+    void getErased();
+
 };
 
 #endif // TRASH_H
