7b88897985e71a7b39f7a28533af3ab72ce0cfe1
logasow <helga15@ru.is>
2015-12-15 21:45:53 +0000

    Lengi er von á einum...

--- famGUI/computer.cpp
+++ famGUI/computer.cpp
@@ -1,31 +1,40 @@
 #include "computer.h"
 
-Computer::Computer()
-{
+Computer::Computer(){
 
 }
+
 Computer::Computer(const int& i, const string& nam,const int& buildY, const string& typ, const bool& bu){
+
     id = i;
     name = nam;
     buildYear = buildY;
     type = typ;
     built = bu;
 }
+
 int Computer::getId()const{
+
     return id;
 }
+
 string Computer::getName()const{
+
     return name;
 }
+
 int Computer::getYear()const{
+
     return buildYear;
 }
 
 string Computer::getType()const{
+
     return type;
 }
 
 bool Computer::getBuilt()const{
+
     return built;
 }
 
