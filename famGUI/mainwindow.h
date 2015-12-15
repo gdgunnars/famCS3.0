b65e017a08269124566f81347421f3fe08b1cc0d
logasow <helga15@ru.is>
2015-12-15 21:44:50 +0000

    Gleymdi einu

--- famGUI/mainwindow.h
+++ famGUI/mainwindow.h
@@ -31,29 +31,29 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_listOfScientist_clicked();
+    void on_listOfScientist_clicked(); //Enables more info on selected scientist and enables edit and delete buttins
 
-    void on_searchName_textChanged();
+    void on_searchName_textChanged(); //Takes in search string and sneds it to domain
 
-    void on_scientistButton_clicked();
+    void on_scientistButton_clicked(); //Switches list to scientist
 
-    void on_computerButton_clicked();
+    void on_computerButton_clicked(); //Switches list to computer
 
-    void on_edit_clicked();
+    void on_edit_clicked(); //Opens up edit window for choosen id
 
-    void on_delete_2_clicked();
+    void on_delete_2_clicked(); //Deletes and removes from list
 
-    void on_trash_clicked();
+    void on_trash_clicked(); //Opens trash
 
-    void on_actionGettingStarted_triggered();
+    void on_actionGettingStarted_triggered(); //Opens link to getting started wiki
 
-    void on_actionAbout_triggered();
+    void on_actionAbout_triggered(); //Opens about logo
 
-    void on_add_clicked();
+    void on_add_clicked(); //Opens up add window
 
-    void on_actionNewScientist_triggered();
+    void on_actionNewScientist_triggered(); //Other way to trigger add window for scientist
 
-    void on_actionNewComputer_triggered();
+    void on_actionNewComputer_triggered(); //Other way to trigger add window for computer
 
 private:
     Ui::MainWindow *ui;
@@ -63,24 +63,24 @@ private:
     bool PC;
 
     //Starting
-    void startingList();
+    void startingList(); //clears search window and shows choosen list
 
     //Display table
-    void displayListPersons(vector<cScientist> a);
+    void displayListPersons(vector<cScientist> a); //Main controller for displaying each scientist in vector a
     QString showGender(char input); //Gets gender as a char and returns QString with gender
     QString showYear(int input); //Gets year as int and returns QString with year
-    void createTablePersons(const int& size);
-    void displayListComputers(vector<Computer> a);
-    QString showBuilt(bool input);
-    void createTableComputers(const int& size);
-    void moreInfo();
-    void connected();
+    void createTablePersons(const int& size); //Creates a empty table of input size for scientists
+    void displayListComputers(vector<Computer> a); //Main controller for displaying each computer in vector a
+    QString showBuilt(bool input); //Changes bool value to built/not built
+    void createTableComputers(const int& size); //Creates a empty table of input size for computers
+    void moreInfo(); //Adds fact and id info on selected scientist/computer
+    void connected(); //Shows connections on selected scientist/computer
 
     //Switch:
-    void switchLists();
+    void switchLists(); //Clears and sets up new list in list switch between computer and scientist list
 
     //Utilities:
-    int getIdFromSelected();
+    int getIdFromSelected(); //Returns id of selected
 
 };
 
